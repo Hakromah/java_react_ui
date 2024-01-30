1d3509219548bcb3b4cf604c2c529973ce4ea20b
Hassan <kkromah12@gmail.com>
2024-01-31 01:43:36 +0300

    front end & back end connected successfully

--- src/component/student/AddStudents.js
+++ src/component/student/AddStudents.js
@@ -1,4 +1,6 @@
+import axios from "axios";
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export const AddStudents = () => {
 	const [student, setStudent] = useState({
@@ -8,10 +10,33 @@ export const AddStudents = () => {
 		department: "",
 	});
 
+	// let destructure the reference varibles and pass them to student state
 	const { firstName, lastName, email, department } = student;
+	const handleInputChange = (e) => {
+		// we spread the student reference veriable and listen to name field in input field
+		// and target the value of it.
+		setStudent({ ...student, [e.target.name]: e.target.value });
+	};
+
+	//let's save the data to DATABASE by appending student to our url
+	const saveStudent = async (e) => {
+		e.preventDefault();
+		await axios.post("http://localhost:1002/students", student, {
+			validateStatus: () => {
+				return true;
+			},
+		});
+		setStudent({
+			firstName: "",
+			lastName: "",
+			email: "",
+			department: "",
+		});
+	};
+
 	return (
 		<div className="col-sm-8 py-2 px-5">
-			<form action="">
+			<form onSubmit={(e) => saveStudent(e)}>
 				<div className="input-group mb-5">
 					<label className="input-group-text" htmlFor="firstName">
 						First Name
@@ -23,6 +48,7 @@ export const AddStudents = () => {
 						id="firstName"
 						required
 						value={firstName}
+						onChange={(e) => handleInputChange(e)}
 					/>
 				</div>
 				<div className="input-group mb-5">
@@ -36,6 +62,7 @@ export const AddStudents = () => {
 						id="lastName"
 						required
 						value={lastName}
+						onChange={(e) => handleInputChange(e)}
 					/>
 				</div>
 				<div className="input-group mb-5">
@@ -49,6 +76,7 @@ export const AddStudents = () => {
 						id="email"
 						required
 						value={email}
+						onChange={(e) => handleInputChange(e)}
 					/>
 				</div>
 				<div className="input-group mb-5">
@@ -62,9 +90,28 @@ export const AddStudents = () => {
 						id="department"
 						required
 						value={department}
+						onChange={(e) => handleInputChange(e)}
 					/>
 				</div>
-				<button type="submit">Submit</button>
+				<div className="row mb-5">
+					<div className="col-sm-2">
+						<button
+							type="submit"
+							className="btn btn-outline-success btn-lg"
+						>
+							Save
+						</button>
+					</div>
+					<div className="col-sm-2">
+						<Link
+							to={"/view-students"}
+							type="submit"
+							className="btn btn-outline-warning btn-lg"
+						>
+							Cancel
+						</Link>
+					</div>
+				</div>
 			</form>
 		</div>
 	);
--- src/component/student/StudentView.js
+++ src/component/student/StudentView.js
@@ -37,7 +37,7 @@ const StudentsView = () => {
 					</tr>
 				</thead>
 
-				<tbody className="text-center">
+				<tbody className="text-start">
 					{students.map((student, index) => (
 						<tr key={student.id}>
 							<th scope="row" key={index}>
